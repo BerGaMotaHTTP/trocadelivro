2ec441e3ec4daceb1ad60ac4176ef3d6ac6eaa78
Daniel Berg Valadares Mota <202412170033@ifto.local>
2025-04-07 20:36:36 -0300

    Mensagem do que foi feito

---
+++ src/trocalivro/src/main/java/com/mycompany/trocalivro/Trocalivro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trocalivro;
+
+/**
+ *
+ * @author 202412170033
+ */
+public class Trocalivro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
