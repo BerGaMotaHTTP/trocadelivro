f45e22e317815e617527445e520f6f84237daa5c
Daniel Berg Valadares Mota <202412170033@ifto.local>
2025-04-14 21:17:26 -0300

    Criei os diretorios para começar a progrmar

---
+++ src/TrocaLivroDesktop/src/main/java/com/mycompany/trocalivrodesktop/TrocaLivroDesktop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trocalivrodesktop;
+
+/**
+ *
+ * @author 202412170033
+ */
+public class TrocaLivroDesktop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
